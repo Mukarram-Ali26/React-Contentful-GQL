fe55af05ee1b47182343a7836dc886d450595ca9
Mukarram Ali <mukarramaliofficial@gmail.com>
2022-04-12 08:03:42 +0500

    Update client.js

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import * as contentful from "contentful"; 
 
 export const Client = contentful.createClient({
-    space: 'ye76zj2fp2vd',
-    accessToken: 'va3MXkrKZCE0taQeatUS_mL5aBdUIRYlA_MB9kzN26s'
+    space: '',
+    accessToken: ''
 }) 
 
 
